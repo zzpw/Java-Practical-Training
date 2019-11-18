3cdd2af6ab776ef3a754d5a042e895ee7a31e04e
zengpengwu <41501184+zzpw@users.noreply.github.com>
2019-11-17 22:55:41 -0800

    Add files via upload

--- controller/LoginServlet.java
+++ controller/LoginServlet.java
@@ -7,18 +7,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.LoginDao;
+
 public class LoginServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		this.doPost(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
+		String userName = req.getParameter("username");
+		String passward = req.getParameter("passward");
+		LoginDao logindao = new LoginDao();
+		if (!logindao.checkUserName(userName)) {
+			// 账户不存在处理
+		} else {
+			if (logindao.checkPassWard(userName, passward)) {
+				// 登陆成功处理
+				resp.sendRedirect("mainForm.jsp");
+			} else {
+				// 密码错误处理
+			}
+		}
 	}
 
 }
