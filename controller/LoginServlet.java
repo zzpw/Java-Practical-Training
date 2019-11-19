db1febabf19c94ba06c6460ae4635edb962213a6
zengpengwu <41501184+zzpw@users.noreply.github.com>
2019-11-18 22:01:45 -0800

    Add files via upload

--- controller/LoginServlet.java
+++ controller/LoginServlet.java
@@ -3,12 +3,14 @@ package controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.LoginDao;
 
+@WebServlet("/LoginServlet")   
 public class LoginServlet extends HttpServlet{
 
 	@Override
@@ -20,15 +22,19 @@ public class LoginServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String userName = req.getParameter("username");
 		String passward = req.getParameter("passward");
+		req.setAttribute("userName", userName);
 		LoginDao logindao = new LoginDao();
-		if (!logindao.checkUserName(userName)) {
-			// 账户不存在处理
+		if (!logindao.checkUserName(userName)) { // 账户不存在处理
+			req.setAttribute("checkUserName", "账户不存在");
+			System.out.println("账户不存在");
+			req.getRequestDispatcher("Login.jsp").forward(req, resp);
 		} else {
-			if (logindao.checkPassWard(userName, passward)) {
-				// 登陆成功处理
+			if (logindao.checkPassWard(userName, passward)) { // 登陆成功处理
 				resp.sendRedirect("mainForm.jsp");
-			} else {
-				// 密码错误处理
+			} else { // 密码错误处理
+				req.setAttribute("checkUserName", null);
+				req.setAttribute("checkPassward", "密码错误");
+				req.getRequestDispatcher("Login.jsp").forward(req, resp);
 			}
 		}
 	}
